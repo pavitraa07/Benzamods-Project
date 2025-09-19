b6fa92bfac3013f81873379d6bd2db1d7ff02f43
Pavitra B S <pavitrabs68@gmail.com>
2025-09-19 20:49:21 +0530

    Deployment

--- frontend/src/components/HeroBanner.jsx
+++ frontend/src/components/HeroBanner.jsx
@@ -240,7 +240,7 @@ function HeroBanner({ shopRef, servicesRef, reviewsRef }) {
   const navContainerStyle = {
     display: "flex",
     gap: "15px",
-    marginLeft: "180px",
+    marginLeft: "150px",
     animation: "fadeInRight 1s ease",
   };
 
